9d2c683b145a71d483bdf51628f64df65568516c
Kuenhwi <kueniyam@naver.com>
2019-12-25 19:14:08 +0900

    자바 공부 1일차
     - 가변 인수
     - break label
     - Long은 숫자 뒤에 L 붙이고, float은 숫자 뒤에 f 붙임

---
+++ src/application/Main.java
@@ -0,0 +1,16 @@
+package application;
+
+import study.A;
+import study.B;
+import study.C;
+
+public class Main {
+    public static void main(String[] args) {
+        // A.test();
+        // B.test();
+        // System.out.println(C.sum(1,2,3,4,5,6,7,8,9,10));
+        // System.out.println(C.sum(new int[]{1,2,3,4,5}));
+
+
+    }
+}
---
+++ src/study/B.java
@@ -0,0 +1,21 @@
+package study;
+
+/**
+ * break label 에 관한 예제
+ */
+public class B {
+    public static void test() {
+        int a = 0;
+        label:
+        while (true) {
+            System.out.println(a);
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i);
+                if (a == 3 && i == 3) {
+                    break label;
+                }
+            }
+            a++;
+        }
+    }
+}
